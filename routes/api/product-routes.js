d712a432a25f04c156ad1fbf8f9b12960bb3e793
Jacob Studstill <jacob.studstill@gmail.com>
2023-02-08 22:39:02 -0500

    Added the post and delete for the product-route.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,9 +23,28 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  try {
+    const products = await Product.findByPk(req.params.id, {
+      include: [Category, {
+        model: Tag,
+        through: ProductTag,
+      }],
+    });
+    //if products isn't valid
+    if (!products) {
+      res.status(404).json({ message: "This product tag is not valid!" });
+      return;
+    }
+    //if products is valid
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
@@ -102,8 +121,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productDelete = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productDelete) {
+      res.status(404).json({ message: "This product tag is not valid!" });
+      return;
+    }
+    res.status(200).json(productDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
