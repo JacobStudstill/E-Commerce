5517e0daa01fb7e7741e46b19dfb8dd3753e3960
Jacob Studstill <jacob.studstill@gmail.com>
2023-02-08 23:12:33 -0500

    Added the post and delete for categiry-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,21 +21,71 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  try {
+    const categorys = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
+    //If categorys is not valid
+    if (!categorys) {
+      res.status(404).json({ message: 'This category ID is not valid!' });
+    }
+    //if categorys is valid
+    res.status(200).json(categorys);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+
+  try {
+    const categorys = await Category.create(req.body);
+    res.status(200).json(categorys);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
+  try {
+    const categorys = await Category.update(req.body, 
+      {
+      where: {
+        id: req.params.id,
+      },
+    });
+    //if categorys is not valid
+    if (!categorys) {
+      res.status(404).json({ message: 'This category ID is not valid!' });
+    }
+    //if categorys is valid
+    res.status(200).json(categorys);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategorys = Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteCategorys);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
